df2b4df450d2dd63b29257d99ee316d90d5ac433
Jack Huang <jackhwl@hotmail.com>
2018-09-01 10:45:09 -0400

    Understanding ASP.NET Core 2.x

---
+++ globoma/Globoma/Startup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using WebApp.Services;
+
+namespace Globoma
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IConferenceService, ConferenceMemoryService>();
+            services.AddSingleton<IProposalService, ProposalMemoryService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILogger<Startup> logger)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Use(async (context, next) =>
+            {
+                //await context.Response.WriteAsync("Hello World!");
+                logger.LogInformation("Before second");
+                await next();
+                logger.LogInformation("After second");
+            });
+
+            app.Run(async (context) =>
+            {
+                logger.LogInformation("second");
+                await context.Response.WriteAsync("Second text!");
+            });
+        }
+    }
+}

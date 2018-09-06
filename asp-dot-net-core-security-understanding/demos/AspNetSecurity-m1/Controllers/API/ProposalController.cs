27e9d95e6cdb89541d986cfa2245b68955d4e2f6
Jack Huang <jackhwl@hotmail.com>
2018-09-05 22:58:51 -0400

    asp.net core security

---
+++ asp-dot-net-core-security-understanding/demos/AspNetSecurity-m1/Controllers/API/ProposalController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using AspNetSecurity_m1.Models;
+using AspNetSecurity_m1.Repositories;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetSecurity_m1.Controllers.API
+{
+    [EnableCors("AllowBankCom")]
+    [Route("api/[controller]")]
+    public class ProposalController
+    {
+        private readonly ProposalRepo repo;
+
+        public ProposalController(ProposalRepo repo)
+        {
+            this.repo = repo;
+        }
+
+        [HttpGet]
+        public IEnumerable<ProposalModel> Get(int conferenceId)
+        {
+            return repo.GetAllApprovedForConference(conferenceId);
+        }
+    }
+}
---
+++ asp-dot-net-core-security-understanding/demos/AspNetSecurity-m1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore;
+
+namespace AspNetSecurity_m1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+        .UseUrls("http://localhost:5001")
+        .UseStartup<Startup>()
+        .Build();
+
+    }
+}

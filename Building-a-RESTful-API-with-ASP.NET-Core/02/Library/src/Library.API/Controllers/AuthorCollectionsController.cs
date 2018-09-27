4f4e62157ab874fbaea683d1075899ea1c9e12f3
Jack Huang <jackhwl@hotmail.com>
2018-09-26 23:49:12 -0400

    create collection of authors

---
+++ Building-a-RESTful-API-with-ASP.NET-Core/02/Library/src/Library.API/Controllers/AuthorCollectionsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Library.API.Entities;
+using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.API.Controllers
+{
+    [Route("api/authorcollections")]
+    public class AuthorCollectionsController : Controller
+    {
+        private ILibraryRepository _libraryRepository;
+
+        public AuthorCollectionsController(ILibraryRepository libraryRepository)
+        {
+            _libraryRepository = libraryRepository;
+        }
+
+        [HttpPost]
+        public IActionResult CreateAuthorCollection([FromBody] IEnumerable<AuthorForCreationDto> authorCollection)
+        {
+            if (authorCollection == null) return BadRequest();
+            var authorEntities = Mapper.Map<IEnumerable<Author>>(authorCollection);
+            foreach (var author in authorEntities)
+            {
+                _libraryRepository.AddAuthor(author);
+            }
+
+            if (!_libraryRepository.Save())
+            {
+                throw new Exception("Creating an author collection failed on save.");
+            }
+
+            return Ok();
+        }
+    }
+}

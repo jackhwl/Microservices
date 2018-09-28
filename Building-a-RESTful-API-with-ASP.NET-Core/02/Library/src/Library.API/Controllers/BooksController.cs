f7efda38e93b81359a2c2e7f9dea2b4467a9d17b
Jack Huang <jackhwl@hotmail.com>
2018-09-27 22:20:06 -0400

    delete resource and children

--- Building-a-RESTful-API-with-ASP.NET-Core/02/Library/src/Library.API/Controllers/BooksController.cs
+++ Building-a-RESTful-API-with-ASP.NET-Core/02/Library/src/Library.API/Controllers/BooksController.cs
@@ -55,5 +55,22 @@ namespace Library.API.Controllers
             var bookToReturn = Mapper.Map<BookDto>(bookEntity);
             return CreatedAtRoute("GetBookFroAuthor", new {authorId, id = bookToReturn.Id}, bookToReturn);
         }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBookForAuthor(Guid authorId, Guid id)
+        {
+            if (!_libraryRepository.AuthorExists(authorId)) return NotFound();
+            var bookForAuthorFromRepo = _libraryRepository.GetBookForAuthor(authorId, id);
+            if (bookForAuthorFromRepo == null) return NotFound();
+
+            _libraryRepository.DeleteBook(bookForAuthorFromRepo);
+            if (!_libraryRepository.Save())
+            {
+                throw new Exception($"Deleting book {id} for author {authorId} failed on save.");
+            }
+
+            return NoContent();
+
+        }
     }
 }

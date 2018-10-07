5513b3a8e9e6afc72f2967a73a48e50599c67b5f
Jack Huang <jackhwl@hotmail.com>
2018-10-06 21:28:41 -0400

    self-discoverability with Root document

--- Building-a-RESTful-API-with-ASP.NET-Core/02/Library/src/Library.API/Controllers/AuthorsController.cs
+++ Building-a-RESTful-API-with-ASP.NET-Core/02/Library/src/Library.API/Controllers/AuthorsController.cs
@@ -30,7 +30,7 @@ namespace Library.API.Controllers
 
         [HttpGet(Name = "GetAuthors")]
         //public IActionResult GetAuthors([FromQuery(Name="page")] int pageNumber = 1, [FromQuery] int pageSize = 10)
-        public IActionResult GetAuthors(AuthorsResourceParameters authorsResourceParameters)
+        public IActionResult GetAuthors(AuthorsResourceParameters authorsResourceParameters, [FromHeader(Name ="Accept")] string mediaType)
         {
             //pageSize = (pageSize > maxAuthorPageSize) ? maxAuthorPageSize : pageSize;
             if (!_propertyMappingService.ValidMappingExistsFor<AuthorDto, Author>(authorsResourceParameters.OrderBy))
@@ -40,43 +40,65 @@ namespace Library.API.Controllers
                 return BadRequest();
 
             var authorsFromRepo = _libraryRepository.GetAuthors(authorsResourceParameters);
-            //var previousPageLink = authorsFromRepo.HasPrevious
-            //    ? CreateAuthorsResourceUri(authorsResourceParameters, ResourceUriType.PreviousPage)
-            //    : null;
-            //var nextPageLink = authorsFromRepo.HasNext
-            //    ? CreateAuthorsResourceUri(authorsResourceParameters, ResourceUriType.NextPage)
-            //    : null;
-            var paginationMetadata = new
-            {
-                totalCount = authorsFromRepo.TotalCount,
-                pageSize = authorsFromRepo.PageSize,
-                currentPage = authorsFromRepo.CurrentPage,
-                totalPages = authorsFromRepo.TotalPages,
-                //previousPageLink,
-                //nextPageLink
-            };
-
-            Response.Headers.Add("X-Pagination", Newtonsoft.Json.JsonConvert.SerializeObject(paginationMetadata));
 
             var authors = Mapper.Map<IEnumerable<AuthorDto>>(authorsFromRepo);
 
-            var links = CreateLinksForAuthors(authorsResourceParameters, authorsFromRepo.HasNext,
-                authorsFromRepo.HasPrevious);
-            var shapedAuthors = authors.ShapeData(authorsResourceParameters.Fields);
-            var shapedAuthorsWithLinks = shapedAuthors.Select(author =>
+            if (mediaType == "application/vnd.marvin.hateoas+json")
             {
-                var authorAsDictionary = author as IDictionary<string, object>;
-                var authorLinks =
-                    CreateLinksForAuthor((Guid) authorAsDictionary["Id"], authorsResourceParameters.Fields);
-                authorAsDictionary.Add("links", authorLinks);
-                return authorAsDictionary;
-            });
-            var linkedCollectionResource = new
+                var paginationMetadata = new
+                {
+                    totalCount = authorsFromRepo.TotalCount,
+                    pageSize = authorsFromRepo.PageSize,
+                    currentPage = authorsFromRepo.CurrentPage,
+                    totalPages = authorsFromRepo.TotalPages,
+                    //previousPageLink,
+                    //nextPageLink
+                };
+
+                Response.Headers.Add("X-Paination", Newtonsoft.Json.JsonConvert.SerializeObject(paginationMetadata));
+
+                var links = CreateLinksForAuthors(authorsResourceParameters, authorsFromRepo.HasNext,
+                    authorsFromRepo.HasPrevious);
+                var shapedAuthors = authors.ShapeData(authorsResourceParameters.Fields);
+                var shapedAuthorsWithLinks = shapedAuthors.Select(author =>
+                {
+                    var authorAsDictionary = author as IDictionary<string, object>;
+                    var authorLinks =
+                        CreateLinksForAuthor((Guid) authorAsDictionary["Id"], authorsResourceParameters.Fields);
+                    authorAsDictionary.Add("links", authorLinks);
+                    return authorAsDictionary;
+                });
+                var linkedCollectionResource = new
+                {
+                    value = shapedAuthorsWithLinks,
+                    links = links
+                };
+
+                return Ok(linkedCollectionResource);
+            }
+            else
             {
-                value = shapedAuthorsWithLinks,
-                links = links
-            };
-            return Ok(linkedCollectionResource);
+                var previousPageLink = authorsFromRepo.HasPrevious
+                    ? CreateAuthorsResourceUri(authorsResourceParameters, ResourceUriType.PreviousPage)
+                    : null;
+                var nextPageLink = authorsFromRepo.HasNext
+                    ? CreateAuthorsResourceUri(authorsResourceParameters, ResourceUriType.NextPage)
+                    : null;
+                var paginationMetadata = new
+                {
+                    totalCount = authorsFromRepo.TotalCount,
+                    pageSize = authorsFromRepo.PageSize,
+                    currentPage = authorsFromRepo.CurrentPage,
+                    totalPages = authorsFromRepo.TotalPages,
+                    previousPageLink,
+                    nextPageLink
+                };
+
+                Response.Headers.Add("X-Paination", Newtonsoft.Json.JsonConvert.SerializeObject(paginationMetadata));
+
+                return Ok(authors.ShapeData(authorsResourceParameters.Fields));
+            }
+
         }
 
         [HttpGet("{id}", Name="GetAuthor")]
@@ -132,7 +154,7 @@ namespace Library.API.Controllers
             }
         }
 
-        [HttpPost]
+        [HttpPost(Name ="CreateAuthor")]
         public IActionResult CreateAuthor([FromBody] AuthorForCreationDto author)
         {
             if (author == null) return BadRequest();
--- Building-a-RESTful-API-with-ASP.NET-Core/02/Library/src/Library.API/Startup.cs
+++ Building-a-RESTful-API-with-ASP.NET-Core/02/Library/src/Library.API/Startup.cs
@@ -41,6 +41,12 @@ namespace Library.API
                 setupAction.ReturnHttpNotAcceptable = true;
                 setupAction.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
                 setupAction.InputFormatters.Add(new XmlDataContractSerializerInputFormatter());
+
+                var jsonOutputFormatter = setupAction.OutputFormatters.OfType<JsonOutputFormatter>().FirstOrDefault();
+                if (jsonOutputFormatter != null)
+                {
+                    jsonOutputFormatter.SupportedMediaTypes.Add("application/vnd.marvin.hateoas+json");
+                }
             }).AddJsonOptions(options => { options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();});
 
             // register the DbContext on the container, getting the connection string from

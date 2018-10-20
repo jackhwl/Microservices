9366e8f04968a0bf592e92cf73282998b6bb1d69
Jack Huang <jackhwl@hotmail.com>
2018-10-19 20:50:00 -0400

    Handling Different Rules Between creating and updating resources (Server)

--- Building Business Applications with Angular and ASP.NET Core/TourManagement.API/Dtos/TourAbstractBase.cs
+++ Building Business Applications with Angular and ASP.NET Core/TourManagement.API/Dtos/TourAbstractBase.cs
@@ -11,7 +11,7 @@ namespace TourManagement.API.Dtos
         [Required(AllowEmptyStrings = false, ErrorMessage = "Title is required.")]
         [MaxLength(200, ErrorMessage = "Title is too long.")]
         public string Title { get; set; }
-        public string Description { get; set; }
+        public virtual string Description { get; set; }
         public DateTimeOffset StartDate { get; set; }
         public DateTimeOffset EndDate { get; set; }
 
--- Building Business Applications with Angular and ASP.NET Core/TourManagement.API/Dtos/TourForUpdate.cs
+++ Building Business Applications with Angular and ASP.NET Core/TourManagement.API/Dtos/TourForUpdate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,7 @@ namespace TourManagement.API.Dtos
 {
     public class TourForUpdate : TourAbstractBase
     {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "When updating a tour, the description is required.")]
+        public override string Description { get; set; }
     }
 }

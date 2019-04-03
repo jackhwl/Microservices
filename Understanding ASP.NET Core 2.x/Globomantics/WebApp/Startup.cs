bdf94f456d3b7cc57a0e404ae9ff2e5b47b84e96
jackhwl <jackhwl@hotmail.com>
2019-04-02 21:04:25 -0400

    Understanding ASP.NET Core 2.x

---
+++ Understanding ASP.NET Core 2.x/Globomantics/WebApp/Startup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using System.Net.Http;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebApp.Services;
+
+namespace WebApp
+{
+    public class Startup
+    {
+        private readonly IConfiguration configuration;
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddSingleton<IConferenceService, ConferenceMemoryService>();
+            services.AddSingleton<IProposalService, ProposalMemoryService>();
+            services.AddSingleton(x => 
+                new HttpClient {BaseAddress = new Uri("http://localhost:5000") });
+
+            services.Configure<GlobomanticsOptions>(configuration.GetSection("Globomantics"));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Conference}/{action=Index}/{id?}");
+            });
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+        }
+    }
+}
---
+++ Understanding ASP.NET Core 2.x/Globomantics/WebApp/ViewComponents/StatisticsViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Services;
+
+namespace WebApp.ViewComponents
+{
+    public class StatisticsViewComponent : ViewComponent
+    {
+        private readonly IConferenceService service;
+
+        public StatisticsViewComponent(IConferenceService service)
+        {
+            this.service = service;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(await service.GetStatistics());
+        }
+    }
+}
